31b3fa5b47a367bcf455a126f455bb24476b9bb4
ItsFire143 <112923603+ItsFire143@users.noreply.github.com>
2022-10-09 19:12:55 +0300

    Ho4u 4ipcov

---
+++ src/Connect.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Connect {
+    public void Connect (String username, String password, String jdbcURL) throws IOException {
+
+        try {
+            java.sql.Connection connection = DriverManager.getConnection(jdbcURL, password, username);
+            System.out.println("Ракеты запущены, это конец, поздравляю, товарищ!");
+            connection.close();
+        } catch (SQLException e){
+            System.out.println("Ракеты не запущены, кажется что-то пошло не так! Пожалуйста, подождите");
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        Connect connect = new Connect();
+        System.out.println("Enter login");
+        String username = scanner.nextLine();
+        System.out.println("Enter password");
+        String password = scanner.nextLine();
+
+        Connect con = new Connect();
+        connect.Connect(username, password, "46.229.214.241/pgadmin4");
+    }
+}
